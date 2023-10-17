bb86e9152992a47698fb8ed5b19574baa0cde06d
WhoAsked9 <134094752+WhoAsked9@users.noreply.github.com>
2023-10-17 19:32:41 +0500

    Create HelloMafaka.lua

---
+++ HelloMafaka.lua
@@ -0,0 +1,12 @@
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.9.16) ~  Much Love, Ferib 
+
+]]--
+
+local v0=loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))();local v1=v0:MakeWindow({Name="Destroy Economy рџ’Ђ",IntroText="I FARTED",HidePremium=false,SaveConfig=true,ConfigFolder="OrionTest"});local v2=v1:MakeTab({Name="CAMAVINGAAAA",Icon="rbxassetid://4483345998",PremiumOnly=false});v2:AddButton({Name="Get Ultamite Pogo stick (req. 100 coins and rejoin)"});function Callback()local v3={[1 + 0 ]="ultamite",[4 -2 ]=719 -(555 + 64) ,[3]="special"};end v0:Init();
